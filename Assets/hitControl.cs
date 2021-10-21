1105dc82f602124b051a58a4858649faaced4922
its-maxfield <maxfield.friedman12@gmail.com>
2021-10-21 14:22:58 -0700

    Uploading the first ieration of the game.

---
+++ Assets/hitControl.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class hitControl : MonoBehaviour
+{
+    //The target object to spawn in the scene.
+    public GameObject targetPrefab;
+
+    //Count the number of missed shots
+    private int missCounter = 0;
+    private int targetsHit = 0;
+
+    private List<GameObject> targets = new List<GameObject>();
+
+    [SerializeField]
+    public int numTargets = 5;
+
+    private float height;
+    private float width;
+
+    private Vector3 origin;
+    public GameObject gameOver;
+
+    void Start()
+    {
+      Camera cam = Camera.main;
+      origin = cam.GetComponent<Transform>().position;
+      height = Mathf.Ceil(((2f * cam.orthographicSize) % 5.0f)) * 1.75f;
+      width = Mathf.Ceil(((height * cam.aspect) % 5.0f)) * 2.5f;
+
+      for(int i = 0; i < numTargets; ++i)
+      {
+        makeTarget();
+      }
+    }
+
+    //Used for the Physics calculations.
+    void Update()
+    {
+      if(Input.GetMouseButtonDown(0) && gameOver.activeSelf == false)
+      {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+        try
+        {
+          //Determine if it is a target layer.
+          if(hit.collider.gameObject.layer == 8)
+          {
+            foreach(Transform child in hit.collider.gameObject.transform)
+            {
+              if(child.gameObject.name.Contains("Hit"))
+              {
+                child.gameObject.SetActive(true);
+              }
+              else
+              {
+                child.gameObject.SetActive(false);
+              }
+            }
+            ++targetsHit;
+
+            StartCoroutine(ReplaceTarget(hit.collider.gameObject));
+            makeTarget();
+          }
+        }
+        catch (NullReferenceException)
+        {
+          Debug.Log("Target not hit!");
+          ++missCounter;
+        }
+      }
+    }
+
+    private void makeTarget()
+    {
+      float targetX = UnityEngine.Random.Range(origin.x - width, origin.x + width);
+      float targetY = UnityEngine.Random.Range(origin.y - height, origin.y + height);
+
+      Instantiate(targetPrefab, new Vector3(targetX, targetY, -1), Quaternion.identity);
+    }
+
+    IEnumerator ReplaceTarget(GameObject toRemove)
+    {
+      yield return new WaitForSeconds(0.2f);
+      Destroy(toRemove);
+    }
+
+    public int returnMisses()
+    {
+      return missCounter;
+    }
+
+    public int returnTargets()
+    {
+      return targetsHit;
+    }
+}
---
+++ Assets/uiControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uiControl : MonoBehaviour
+{
+    public Text timerText;
+    public GameObject gameOver;
+    public Text gameOverInfo;
+
+    private float timer = 20.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+      float seconds = timer % 60;
+      timerText.text = "Time Left: " + seconds.ToString("n2");
+
+      if(timer > 0.0f)
+      {
+        timer -= Time.deltaTime;
+      }
+      else
+      {
+        timerText.text = "";
+        endGame();
+      }
+    }
+
+    private void endGame()
+    {
+      Debug.Log("Ending game");
+
+      gameOver.SetActive(true);
+      Time.timeScale = 0f;
+
+      float misses = (float)gameObject.GetComponent<hitControl>().returnMisses();
+      float hits = (float)gameObject.GetComponent<hitControl>().returnTargets();
+
+      float totalShots = misses + hits;
+
+      float percentMiss = (misses / totalShots) * 100f;
+      float percentHit = (hits /totalShots) * 100f;
+
+      string p1 = "You clicked a total of " + totalShots.ToString() + " pumpkins.\n";
+      string p2 = "You missed " + misses.ToString() + " of the pumpkins you tried to click.\n";
+      string p3 = "You clicked a pumpkin without missing " + hits.ToString() + " times.\n\n";
+      string p4 = "Your miss percentage was: " + percentMiss.ToString("n2") + " percent\n";
+      string p5 = "Your click percentage was " + percentHit.ToString("n2") + " percent.";
+
+      gameOverInfo.text = p1 + p2 + p3 + p4 + p5;
+    }
+}
